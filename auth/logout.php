df56fdcf590cbe6145b5d2e2a1c4130626f752d1
HussienTalha <hussien.talha99@gmail.com>
2025-12-10 19:52:39 +0200

    logout opration

--- auth/logout.php
+++ auth/logout.php
@@ -1,8 +1,12 @@
 <?php
 
+if (session_status() !== PHP_SESSION_ACTIVE)
+{
 session_start();
+}
+require_once __DIR__."/../core/config.php";
 if (isset($_SESSION['account']))
 	{
 		session_destroy();
 	}
-header("location: /../views/home.php");
+header("location: /views/home.php");
