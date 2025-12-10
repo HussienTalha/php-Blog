53e7b2c7db7b59b6ea08868dfd6f2e3d1328723a
HussienTalha <hussien.talha99@gmail.com>
2025-12-10 19:49:49 +0200

    adminstration actions on posts

--- admin/post.php
+++ admin/post.php
@@ -1,6 +1,10 @@
 <?php
+if (session_status() !== PHP_SESSION_ACTIVE)
+{
+session_start();
+}
 require_once __DIR__."/../core/DB.php";
-use pdo;
+require_once __DIR__."/../core/config.php";
 
 class Post 
 {
