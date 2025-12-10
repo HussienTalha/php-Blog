2334969e45f7a989206f1c04a8854e18377564d1
HussienTalha <hussien.talha99@gmail.com>
2025-12-10 19:41:04 +0200

    header layout

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,13 +1,20 @@
 <?php
-define("Root","http://localhost:8000");
+if (session_status() !== PHP_SESSION_ACTIVE)
+{
+session_start();
+}
+require_once __DIR__."/../../core/config.php";
+
 ?>
 <html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form</title>
+    <title>Blog</title>
     <!-- Add Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Add Custom CSS -->
+    <link href="<?=Root;?>/public/css/style.css" rel="stylesheet">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
@@ -19,28 +26,40 @@ define("Root","http://localhost:8000");
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-	<a class="nav-link active" aria-current="page" href=<?=Root;?>>Home</a>
+	<a class="nav-link active" aria-current="page" href="<?=Root;?>">Home</a>
         </li>
-        <li class="nav-item">
-	<a class="nav-link active" href="<?=Root?>/views/login.php">Login</a>
-        </li>
+<?php if( isset ($_SESSION['account'])):?>
+	<li class="nav-item">
+	<a class="nav-link active" href="<?=Root?>/views/profile.php">Profile</a>
+	</li>
+	<li class=nav-item">
+<?php endif;?>
+	<a class="nav-link active" href="<?=Root?>/views/categories.php">Categories</a>
+	</li>
+<?php if(isset ($_SESSION['account']['admin'])):?>
+	<li class=nav-item">
+	<a class="nav-link active" href="<?=Root?>/views/dashboard.php">Dashboard</a>
+	</li>
+<?php endif;?>
+      </ul>
+	<ul class="navbar-nav">	
+<?php if(! isset ($_SESSION['account'])):?>
         <li class="nav-item">
 	<a class="nav-link active" href="<?=Root?>/views/register.php">Register</a>
 	</li>
-	<li class="nav-item">
-	<a class="nav-link active" href="<?=Root?>/views/profile.php">Profile</a>
+        <li class="nav-item">
+	<a class="nav-link active" href="<?=Root?>/views/login.php">Login</a>
         </li>
-	<li class=nav-item">
-	<a class="nav-link active" href="<?=Root?>/views/categories.php">Categories</a>
-
+<?php else :?>
 	</li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+	<li class="nav-item">
+	<a class="nav-link active" href="<?=Root?>/auth/logout.php">Logout</a>
+<?php endif;?>
+	</li>
+
+	</ul>
     </div>
   </div>
   </nav>
-</body>
-</html>
+  
+<div class="container">
