9d7de7cbec4d2fbad9b02bb20b2eb5c5b053b8c0
HussienTalha <hussien.talha99@gmail.com>
2025-12-10 19:50:22 +0200

    adminstration actions on users

--- admin/users.php
+++ admin/users.php
@@ -1,14 +1,18 @@
 <?php
-require_once __DIR__.'/../core/DB.php';
-
-use pdo;
-
-class users
+if (session_status() !== PHP_SESSION_ACTIVE)
 {
-	session_start();
+session_start();
+}
+require_once __DIR__.'/../core/DB.php';
+require_once __DIR__."/../core/config.php";
+
+class Users
+{
 	public DB $db;
 	public pdo $pdo;
 	public int $id;
+
+	public array $errors;
 
 	public function __construct()
 	{
@@ -16,15 +20,14 @@ class users
 		$this->pdo = $this->db->getConnection();
 	}
 
-	public function validateAdmin() //make default value the id from the session
+	public function validateAdmin($username) //make default value the id from the session
 	{
-		$this->id = $SESSION['account']['admin'];
-		$query = "SELECT admin FROM users WHERE ID = :id";
+		$query = "SELECT admin FROM users WHERE username = :username";
 		$stmt = $this->pdo->prepare($query);
 		$stmt->execute
 			(
 				[
-					'id' => $this->id
+					'username' => $username
 				]
 			);
 		$admin = $stmt->fetch();
@@ -52,96 +55,225 @@ class users
 	}
 
 
-	public function createUser($userName, $email, $passwd, $admin)
+
+	public function createUser()
 	{	
-		if ($this->validateAdmin()) 
-		{	
-			
-			$username = trim($username);
-			$email = trim($email);
-			$passwd = trim($passwd);
-			$confirmPW = trim($confirmPW);
+		$username = trim($_POST['username']);
+		$email = trim($_POST['email']);
+		$passwd =trim($_POST['password']);
+		$confirmPW = trim($_POST['confirmPassword']);
+		$admin = trim($_POST['admin']);
 
-			if ($passwd !== $confirmPW)
-			{
-				$_SESSION['state'] = 'enter the password correctly';
-				header("location : ../views/register.php");
-				return;
-			}
-			$passwd = password_hash($passwd, PASSWORD_DEFAULT);
-		
-			try
-			{
-				$query = "INSERT INTO users (username, email, password, admin) values (:username, :email, :password, :admin)";
-				$stmt = $this->pdo->prepare($query);
-				$stmt->execute
-					(
-							[
-							'username' => $userName,
-							'email' => $email,
-							'password' => $passwd,
-							'admin' => $admin
-						]
-					);
-				$_SESSION['state'] = 'user created successully';
-				header("location : ../views/dashboard";
-				return;
-			}
-			catch (PDOException $e)
-			{
-				$_SESSION['state'] ="unexpected error";
-				header("location : ../views/dashboard";
-				return;	
-			}
-		else
-			{
-				$_SESSION['state'] = "you're not authorized to create new user";
-				header("location : ../views/dashboard";
-				return;
-			}
-
+		$this->validatePassword($passwd, $confirmPW);
+		$this->validateEmail($email);
+		$this->validateUsername($username);
+		if (! empty ($_SESSION['validationError']))
+		{
+			header ("location: createUser.php");
+			exit ;
 		}
+		$passwd = password_hash($passwd, PASSWORD_DEFAULT);
+
+		try
+		{
+			$query = "INSERT INTO users (username, email, password, admin) values (:username, :email, :password, :admin)";
+			$stmt = $this->pdo->prepare($query);
+			$stmt->execute
+				(
+						[
+						'username' => $username,
+						'email' => $email,
+						'password' => $passwd,
+						'admin' => $admin
+						]
+				);
+			return 'user created successully';
+		}
+		catch (PDOException $e)
+		{
+			return "unexpected error";
+		}
+	}
 	public function deleteUser($username)
 	{
-		$userId = $this->getUserId($username);
 
-		if ($this->validateAdmin())
-		{
-			if ($this->validateAdmin($userId))
+			if(! isset($_SESSION['account']['admin']) )
 			{
-				$_SESSION['state'] ="not authorized to delete admin's profile";
-				header("location : ../views/dashboard";
-				return;
+
+				return "not authorized to delete users";
+			}
+			if (($this->validateAdmin($username)) && ! isset($_SESSION['account']['superAdmin']))
+			{
+
+				return "not authorized to delete admin's profile";
 			}
 			else
 			{
 			try
 			{
-				$query = "DELETE FROM users WHERE ID = :id";
+				$query = "DELETE FROM users WHERE username = :username";
 				$stmt = $this->pdo->prepare($query);
 				$stmt->execute
 					(
 						[
-							'id' => $userId
+							'username' => $username
 						]
 					);
-				$_SESSION['state'] = "users is deleted";
-				header("location : ../views/dashboard";
-				return;
+				
+				if ($stmt->rowCount() > 0)
+				{
+					return "User $username is deleted";
+				}
+				else 
+				{
+					return "couldn't find the username $username";
+				}
 			}
 			catch (PDOException $e)
 			{
-				 $_SESSION['state'] ="unexpected error $e";
-				 header("location : ../views/dashboard";
-				 return;
+				 return "unexpected error $e";
 			}
 			}
-		}
-		else 
+	}
+	
+	public function getAdmins()
+	{
+		$query = "SELECT * FROM users WHERE admin = 1";
+		$stmt = $this->pdo->prepare($query);
+		$stmt->execute();
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+	}
+
+ 	public function getUsers()
+	{
+		$query = "SELECT * FROM users";
+		$stmt = $this->pdo->prepare($query);
+		$stmt->execute();
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+	}
+
+	public function validatePassword($password, $confirmPassWD)
+	{	
+		if (! isset($_POST['password']))
 		{
-			$_SESSION['state'] = "not authorized to delete user";
-			header("location : ../views/dashboard";
+        $this->errors['password'] = "Enter a password";
+        $_SESSION['validationError'] = $this->errors;
+        return ;
+    	}
+		if (! isset($_POST['confirmPassword']))
+		{
+			$this->errors['password'] = "confirm the password";
+			 $_SESSION['validationError'] = $this->errors;
+			 return;
+
+		}
+		if ($password !== $confirmPassWD)
+		{
+			$this->errors['password'] = 'enter the password correctly';
+			 $_SESSION['validationError'] = $this->errors;
+
+			 return;
+		}
+		if (strpbrk($password ,"';\\<>&%!|#$") !== false)
+		{
+			$this->errors['password'] = " password can't have any of these chrchters \"';\\<>&%!|#$";
+			 $_SESSION['validationError'] = $this->errors;
+			 return;
+
+		}
+		if (strlen($password)< 6)
+		{
+			$this->errors['password'] = "password is too short must be at least 6" ;
+			 $_SESSION['validationError'] = $this->errors;
+			 return;
+
+		}
+
+		if (strlen($password)>16)
+		{
+			$this->errors['password'] = "password is too long" ;
+			 $_SESSION['validationError'] = $this->errors;
+			 return;
+
+		}
+
+	}
+	public function validateEmail($email)
+	{
+		if (! isset($_POST['email']))
+		{
+			$this->errors['email'] = "email required";
+			$_SESSION['validationError'] = $this->errors;
+			return;
+
+		}
+		if (! filter_var($email, FILTER_VALIDATE_EMAIL))
+		{
+			$this->errors['email'] = "enter valid email";
+			$_SESSION['validationError'] = $this->errors;;
 			return;
 		}
+		$query = "SELECT email FROM users WHERE email = :email";
+		$stmt = $this->pdo->prepare($query);
+		$stmt->execute
+			(
+				[
+					'email' => $email
+				]
+			);
+		if (($stmt->fetch()) !=false)
+		{
+			$this->errors['email'] = "email already exist";
+			$_SESSION['validationError'] = $this->errors;;
+			return;
 		}
+		if (strpbrk($email , "\"';\\<>&%!|#$") !== false)
+		{
+			$this->errors['email'] = "email can't have these \"';\\<>&%!|#$ charchters";
+			$_SESSION['validationError'] = $this->errors;;
+			return;
+		}
+
+
+		return true;
+	
 	}
+	public function validateUsername($username)
+	{	
+		if (! isset($_POST['username']))
+		{
+				$this->errors['username'] = "email required";
+				 $_SESSION['validationError'] = $this->errors;
+				 return;
+				
+		}
+		if (strpbrk($username , "\"';\\<>&%!|#$") !== false)
+		{
+			$this->errors['username'] = "username can't have these \"';\\<>&%!|#$ charchters";
+			 $_SESSION['validationError'] = $this->errors;
+			 return;
+		}
+
+		$query = "SELECT username FROM users WHERE username = :username";
+		$stmt = $this->pdo->prepare($query);
+		$stmt->execute
+			(
+				[
+					'username' => $username
+				]
+			);
+		if (($stmt->fetch()) != false)
+		{
+			$this->errors['username'] = "username already exist";
+			 $_SESSION['validationError'] = $this->errors;
+			 return;
+		}
+	if (strlen($username)>16)
+		{
+			$errors['username'] = "username is too long" ;
+			 $_SESSION['validationError'] = $this->errors;
+			 return;
+		}
+		return true;
+	}
+}
