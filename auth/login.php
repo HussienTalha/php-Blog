27b772a060061049d820f306199f4546bcbf67a7
HussienTalha <hussien.talha99@gmail.com>
2025-12-10 19:52:15 +0200

    login operation and validation

--- auth/login.php
+++ auth/login.php
@@ -1,7 +1,11 @@
 <?php
+if (session_status() !== PHP_SESSION_ACTIVE)
+{
 session_start();
+}
 
 require_once __DIR__.'/../core/DB.php';
+require_once __DIR__."/../core/config.php";
 
 class Login
 {
